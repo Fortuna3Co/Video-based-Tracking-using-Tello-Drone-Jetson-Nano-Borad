41a236fa8371059d515ef0974d8323564b49106d
Fortuna3Co <78258412+Fortuna3Co@users.noreply.github.com>
2021-11-27 14:38:48 +0900

    Add files via upload

--- DroneProgram.py
+++ DroneProgram.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy as np
+import datetime
+import os
 from time import sleep
 from djitellopy import tello
 from cvzone.HandTrackingModule import HandDetector
+from pytz import timezone
+from PIL import Image
 
 ######################################## Tello Drone Face Tracking & Recognition
 def tello_init():
     w, h = 800, 600
     pxError, pyError = 0, 0
     fbRange = [10000, 11000]
-    pid = [0.3, 0.3, 0]
+    pid = [0.1, 0.1, 0]
     return w, h, pxError, pyError, fbRange, pid
 
 def TelloDrone_Connect():
@@ -79,6 +83,9 @@ def trackFace(info, w, h, pid, px_Error, py_Error, fbRange):
     return x_error, y_error
 
 def findFaceWithRecognition(img):
+    
+    recognizer.read('trainer/trainer.yml') # set Recognizer
+    
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(imgGray, 1.2, 8, minSize = (int(360 * 0.05), int(240 * 0.05)))
@@ -112,7 +119,70 @@ def findFaceWithRecognition(img):
     else:
         return img, [[0, 0], 0]
 
+def making_dataset():
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    cap = cv2.VideoCapture(0)
 
+    w, h = 360, 240
+    cap.set(3, w)
+    cap.set(4, h)
+
+    face_id = input('Input face ID : ')
+
+    # 구분 ID 날짜로 바꿀 것
+    count = 1
+    while True:
+        _, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5, minSize=(20, 20))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle (img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            date = str(datetime.datetime.now(timezone("UTC")).astimezone(timezone("Asia/Seoul")).strftime("%m%d%H%M"))
+            count += 1
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + '.' + date + '.jpg', gray[y:y+h,x:x+w])
+            print("dataset/User." + str(face_id) + '.' + str(count) + '.' + date + '.jpg', " was stored!")
+            roi_gray = gray[y : y+h, x : x+w]
+            roi_color = img[y : y+h, x : x+w]
+        cv2.imshow('video', img)
+
+        k = cv2.waitKey(30) & 0xFF
+        if k == 27:
+            break
+        elif count >= 30:
+            break
+
+    cap.release()
+    cv2.destroyWindow('video')
+
+def train_dataset():
+    path = 'dataset'
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+    def getImageAndLabels(path):
+        imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+        faceSamples = []
+        ids = []
+
+        for imagePaths in imagePaths:
+            PIL_img = Image.open(imagePaths).convert('L')
+            img_numpy = np.array(PIL_img, 'uint8')
+            id = int(os.path.split(imagePaths)[-1].split('.')[1])
+            faces = detector.detectMultiScale(img_numpy)
+
+            for (x, y, w, h) in faces:
+                faceSamples.append(img_numpy[y : y+h, x : x+w])
+                ids.append(id)
+        
+        return faceSamples, ids
+
+    faces, ids = getImageAndLabels(path)
+    recognizer.train(faces, np.array(ids))
+
+    recognizer.write('trainer/trainer.yml')
+    print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
 #######################################
 
 start_menu_select_flag = 0
@@ -124,10 +194,13 @@ webcam_w, webcam_h = 1280, 720
 detector = HandDetector(detectionCon=1)
 start_menu_info, faceTracking_menu = [], []
 faceTrackingRecognition_menu, directControl_menu = [], []
+webCamDataset_menu = []
 
 names = ['None', 'KHS', 'KSY', 'LDS'] # For Recognition ID
 recognizer = cv2.face.LBPHFaceRecognizer_create() 
 recognizer.read('trainer/trainer.yml') # set Recognizer
+font = cv2.FONT_HERSHEY_SIMPLEX # print Recgniition ID
+
 
 ## Create class for display
 class display_menu():
@@ -140,7 +213,8 @@ class display_menu():
 start_menu_info.append(display_menu([40, 50], '1. Face Tracking'))
 start_menu_info.append(display_menu([40, 90], '2. Face Tracking with Recognition'))
 start_menu_info.append(display_menu([40, 130], '3. Direct Control'))
-start_menu_info.append(display_menu([40, 170], '4. Quit'))
+start_menu_info.append(display_menu([40, 170], '4. WebCam Dataset'))
+start_menu_info.append(display_menu([40, 210], '5. Quit'))
 ##
 
 ## Face Tracking Menu Class append
@@ -152,7 +226,20 @@ faceTrackingRecognition_menu.append(display_menu([40, 600], 'Quit this mode'))
 ##
 
 ## Direct Control Menu Class append
-directControl_menu.append(display_menu([40, 600], 'Quit this mode'))
+directControl_menu.append(display_menu([50, 200], 'OFF/ON'))
+directControl_menu.append(display_menu([50, 350], '  UP  '))
+directControl_menu.append(display_menu([50, 450], ' DOWN '))
+directControl_menu.append(display_menu([1000, 350], ' LEFT '))
+directControl_menu.append(display_menu([1100, 350], ' RIGHT '))
+directControl_menu.append(display_menu([1050, 250], '  FW  '))
+directControl_menu.append(display_menu([1050, 450], '  BW  '))
+directControl_menu.append(display_menu([40, 600], ' Quit '))
+##
+
+## WebCam Dataset Menu Class append
+webCamDataset_menu.append(display_menu([40, 90], '1. Collect Dataset'))
+webCamDataset_menu.append(display_menu([40, 130], '2. Recognition'))
+webCamDataset_menu.append(display_menu([40, 650], 'Quit this mode'))
 ##
 
 # Initialize webcam camera with width & height
@@ -200,7 +287,6 @@ def display_menu(img, menu_info, lmList, detector):
     for index, menu in enumerate(menu_info, 0):
         x, y = menu.pos
         w, h = menu.size
-    
         cv2.putText(img, menu.text, menu.pos, cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
         
         if lmList:
@@ -211,6 +297,31 @@ def display_menu(img, menu_info, lmList, detector):
                     
                     if l < 50:
                         cv2.putText(img, menu.text, menu.pos, cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
+                        print('Clicked : ', index + 1)
+                        
+                        return img, index + 1
+
+    return img, 0
+
+# display direct control menus
+def direct_control_display_menu(img, menu_info, lmList, detector):
+    
+    for index, menu in enumerate(menu_info, 0):
+        x, y = menu.pos
+        w, h = menu.size
+        text_size = len(menu.text)
+        
+        cv2.putText(img, menu.text, menu.pos, cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        cv2.circle(img, (x + text_size * 10, y - 10), 50, (0, 0, 0))
+        
+        if lmList:
+            if x < lmList[8][0] < x + text_size * 20 and y - 40 < lmList[8][1] < y + 40:
+                    cv2.circle(img, (x + text_size * 10, y - 10), 50, (0, 255, 0), 8)
+                    
+                    l, _, _, = detector.findDistance(8, 12, img, draw=False)
+                    
+                    if l < 50:
+                        cv2.circle(img, (x + text_size * 10, y - 10), 50, (0, 255, 255), 8)
                         print('Clicked : ', index + 1)
                         
                         return img, index + 1
@@ -263,6 +374,10 @@ while True:
 
     ####################### Face Tracking with Recognition Mode On
     elif start_menu_select_flag == 2:
+        
+        me = TelloDrone_Connect()
+        TelloDrone_takeOff(me)
+        
         while True:
             success, img = cap.read()
             img = cv2.flip(img, 1)
@@ -270,7 +385,7 @@ while True:
             lmList, bboxInfo = detector.findPosition(img)
             
             cv2.putText(img, "<Face Tracking & Recognition Mode On>", (40, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
-            img, faceTrackingRecognition_menu_select_flag = display_menu(img, faceTracking_menu, lmList, detector)
+            img, faceTrackingRecognition_menu_select_flag = display_menu(img, faceTrackingRecognition_menu, lmList, detector)
             
             tello_w, tello_h, pxError, pyError, fbRange, pid = tello_init()
             
@@ -292,6 +407,11 @@ while True:
 
     ####################### Direct Control Mode On
     elif start_menu_select_flag == 3:
+        
+        me = TelloDrone_Connect()
+        TelloDrone_takeOff(me)
+        takeOff_flag = 1
+        
         while True:
             
             success, img = cap.read()
@@ -300,17 +420,102 @@ while True:
             lmList, bboxInfo = detector.findPosition(img)
             
             cv2.putText(img, "<Direct Control Mode On>", (40, 50), cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 255), 4)
-            img, directControl_menu_select_flag = display_menu(img, faceTracking_menu, lmList, detector)
+            img, directControl_menu_select_flag = direct_control_display_menu(img, directControl_menu, lmList, detector)
             
-            if directControl_menu_select_flag == 1:
+            lr, fb, ud, yv = 0, 0, 0, 0
+            speed = 60
+            
+            tello_w, tello_h, pxError, pyError, fbRange, pid = tello_init()
+            
+            tello_image = me.get_frame_read().frame
+            tello_image = cv2.resize(tello_image, (tello_w, tello_h))
+            
+            if directControl_menu_select_flag == 1: #takeOff
+                if takeOff_flag: 
+                    me.land()
+                    takeOff_flag = 0
+                else: 
+                    TelloDrone_takeOff(me)
+                    takeOff_flag = 1
+            
+            elif directControl_menu_select_flag == 2: # up
+                ud = speed
+                
+            elif directControl_menu_select_flag == 3: # DOWN
+                ud = -speed
+            
+            elif directControl_menu_select_flag == 4: # LEFT
+                lr = -speed
+                
+            elif directControl_menu_select_flag == 5: # RIGHT
+                lr = speed
+                
+            elif directControl_menu_select_flag == 6: # FW
+                fb = speed
+                
+            elif directControl_menu_select_flag == 7: # BW
+                fb = -speed
+            
+            me.send_rc_control(lr, fb, ud, yv)
+            
+            if directControl_menu_select_flag == 8: # QUIT
                 directControl_menu_select_flag = 0
                 start_menu_select_flag = 0
+                me.land()
                 break    
-                
+            
             cv2.imshow("TelloDrone Program", img)
+            cv2.imshow("Direct Mode", tello_image)
             cv2.waitKey(1)
     
     elif start_menu_select_flag == 4:
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        img = detector.findHands(img)
+        lmList, bboxInfo = detector.findPosition(img)
+            
+        cv2.putText(img, "<Webcam Dataset Mode On>", (40, 50), cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 255), 4)
+            
+        while True:
+            
+            success, img = cap.read()
+            img = cv2.flip(img, 1)
+            img = detector.findHands(img)
+            lmList, bboxInfo = detector.findPosition(img)
+            
+            cv2.putText(img, "<Direct Control Mode On>", (40, 50), cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 255), 4)
+
+            
+            img, webCamDataset_menu_select_flag = display_menu(img, webCamDataset_menu, lmList, detector)
+            print(webCamDataset_menu_select_flag)
+            
+            if webCamDataset_menu_select_flag == 1:
+                making_dataset()
+                train_dataset()
+                
+            
+            elif webCamDataset_menu_select_flag == 2:
+                while True:
+                    success, img = cap.read()
+                    img = cv2.flip(img, 1)
+                    img, _ = findFaceWithRecognition(img)
+                    cv2.imshow("Recognition", img)
+                    
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        webCamDataset_menu_select_flag = 0
+                        cv2.destroyWindow("Recognition")
+                        break
+            
+            elif webCamDataset_menu_select_flag == 3: # QUIT
+                webCamDataset_menu_select_flag = 0
+                start_menu_select_flag = 0
+                break    
+            
+            cv2.imshow("TelloDrone Program", img)
+            cv2.waitKey(1)
+            
+    
+    elif start_menu_select_flag == 5:
         break    
     
     cv2.imshow("TelloDrone Program", img)
@@ -326,3 +531,7 @@ while True:
 # 1. recognition을 진행할 수 있는 웹 캠 데이터셋 모으기
 # 1-2. 모은 데이터셋으로 얼굴 학습
 # 1-3. recognition 수행
+
+# 2. 촬용 기능
+
+# 3. tello_image.release() 
